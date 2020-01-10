4fcc730315407fb7ebf0e168eb0b68409cbec1a8
kudoh <noboru.kudoh@gmail.com>
2020-01-10 15:45:09 +0900

    support statefulset undo operation

--- pkg/k8s/kubectl/undo_command.go
+++ pkg/k8s/kubectl/undo_command.go
@@ -10,6 +10,8 @@ import (
 	"strconv"
 )
 
+const toRevisionName = "to_revision"
+
 type undoCommandFactory struct{}
 
 var _ CommandFactory = &undoCommandFactory{}
@@ -18,9 +20,10 @@ func (*undoCommandFactory) create(config *CommandConfig) (commands []*Command, e
 	factory := createKubectlFactory(config)
 
 	for _, resource := range config.Resources {
+		command := rollout.NewCmdRolloutUndo(factory, config.Streams)
+		var args []string
 		switch {
 		case k8s.IsDeployment(resource.Kind):
-			command := rollout.NewCmdRolloutUndo(factory, config.Streams)
 			d, err := config.Clientset.AppsV1().Deployments(config.Namespace).Get(resource.Name, metav1.GetOptions{})
 			if err != nil {
 				return nil, err
@@ -33,16 +36,28 @@ func (*undoCommandFactory) create(config *CommandConfig) (commands []*Command, e
 				log.Fatalf("%s has rev 1, so undo operation cannot execute.\n", resource.Name)
 			}
 			log.Printf("[%s]current rev:%d\n", resource.Name, rev)
-			setFlag(command, "to-revision", strconv.FormatInt(rev-1, 10))
-			args := []string{fmt.Sprintf("%s/%s", "deployment", resource.Name)}
+			setFlag(command, toRevisionName, strconv.FormatInt(rev-1, 10))
+			args = []string{fmt.Sprintf("%s/%s", "deployment", resource.Name)}
 
-			commands = append(commands, &Command{command: command, args: args})
 		case k8s.IsStatefulSet(resource.Kind):
-			log.Printf("[Warning] Sorry, currently undo operation does not support StatefulSet[%s]. skip...\n", resource.Name)
-			continue
+			sts, err := config.Clientset.AppsV1().StatefulSets(config.Namespace).Get(resource.Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, err
+			}
+			rev, err := config.Clientset.AppsV1().ControllerRevisions(config.Namespace).Get(sts.Status.CurrentRevision, metav1.GetOptions{})
+			if err != nil {
+				return nil, err
+			}
+			if rev.Revision == 1 {
+				log.Fatalf("%s has rev 1, so undo operation cannot execute.\n", resource.Name)
+			}
+			log.Printf("[%s]current rev:%d\n", resource.Name, rev)
+			setFlag(command, toRevisionName, strconv.FormatInt(rev.Revision-1, 10))
+			args = []string{fmt.Sprintf("%s/%s", "statefulset", resource.Name)}
 		default:
 			log.Fatalln("unsupported resource kind", resource.Kind)
 		}
+		commands = append(commands, &Command{command: command, args: args})
 	}
 	return
 }
